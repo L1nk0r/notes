ef1da9092939a32acea02b005cbd9f73859e2013
L1nk0r <1kozlov1bs41@gmail.com>
2022-03-18 10:00:39 +0300

    Added header decoration in notice

--- scripts/main.js
+++ scripts/main.js
@@ -8,17 +8,36 @@ function add_notice(){
    if (notice != ""){
       var newNotice = document.createElement("div");
       newNotice.classList.add("notice");
-      newNotice.innerHTML = notice;
+
+      var ready_notice = parse_notice(notice);
+
+      newNotice.innerHTML = ready_notice;
 
       cont.appendChild(newNotice);
       newNotice.addEventListener("click", function(){remove_notice(this)});
    }
+
+   
 
    noticeInput.value = "";
 }
 
 function remove_notice(item){
    item.remove();
+}
+
+function parse_notice(notice){
+   let strings = notice.split('\n');
+   let result = "";
+   strings.forEach(string => {
+      if (string[0] === '#'){
+         result += "<h2>" + string + "</h2>\n";
+      } else {
+         result += "<p>" + string + "\n";
+      }
+   });
+
+   return result;
 }
 
 btn.addEventListener("click", function(){add_notice()});
